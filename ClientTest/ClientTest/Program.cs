3f690330933c933573b45640ea936c13e8bcfb13
shuangfanguang <695166116@qq.com>
2017-12-30 12:37:02 +0800

    submit

---
+++ ClientTest/ClientTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server2 = new ServiceReference1.UserAuthSoapClient();
+            var res = server2.Auth("asdasd", "asdasd");
+
+            var res2 = server2.Auth("csy", "123456");
+
+            Console.Read();
+        }
+    }
+}
